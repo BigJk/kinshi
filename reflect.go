44ec75d8e70b1424d66ce2e558eb31465f53e135
Daniel S <info@daniel-schmidt.me>
2021-02-23 03:21:28 +0100

    Removed old reflection function.

--- reflect.go
+++ reflect.go
@@ -12,25 +12,6 @@ func getTypeName(s interface{}) string {
 	return reflect.TypeOf(s).Name()
 }
 
-func hasType(s interface{}, t interface{}) error {
-	if reflect.TypeOf(s).Elem().Kind() != reflect.Struct {
-		return fmt.Errorf("target wasn't a struct")
-	}
-
-	var name string
-	if reflect.TypeOf(t).Kind() == reflect.Ptr {
-		name = reflect.TypeOf(t).Elem().Name()
-	} else {
-		name = reflect.TypeOf(t).Name()
-	}
-
-	if !reflect.ValueOf(s).Elem().FieldByName(name).IsValid() {
-		return fmt.Errorf("struct doesn't have type")
-	}
-
-	return nil
-}
-
 func fetchPtrOfType(s interface{}, typeName string) (interface{}, error) {
 	if reflect.TypeOf(s).Elem().Kind() != reflect.Struct {
 		return nil, fmt.Errorf("target wasn't a struct")
